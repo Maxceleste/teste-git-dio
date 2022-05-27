fc0fda551fc6c0cc449f271b366c89b58e932452
Maxceleste <maxpauloteixeira@gmail.com>
2022-05-27 16:39:19 -0300

    Corrigindo letras para caracteres

--- little_code.py
+++ little_code.py
@@ -1,4 +1,4 @@
 print('Olá bem vindo!')
 nome = input('Digite seu nome: ')
 print(f'Gostei do seu nome, {nome}')
-print(f'Seu nome tem {len(nome)} letras!')
+print(f'Seu nome tem {len(nome)} caracteres!')
